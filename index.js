984c3481d9ce52a0cd09e2220d6ed45b70d57c3f
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-06-25 10:58:50 +0500

    .

---
+++ index.js
@@ -0,0 +1,18 @@
+// Obyektlar aloqador masalala.
+
+//1-masala
+// const person = {
+//   name: "Asadbek",
+//   age: 17,
+//   job: "Developer",
+// };
+// console.log(person);
+
+//2-masala
+const person = {
+  name: "Asadbek",
+  age: 17,
+  job: "Developer",
+};
+person.name = "Vali";
+console.log(person);
