1f99688bf339d9f6fc6fcfa3f7f9e33d0b792506
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-06-26 15:35:53 +0500

    ..

--- index.js
+++ index.js
@@ -9,10 +9,97 @@
 // console.log(person);
 
 //2-masala
-const person = {
-  name: "Asadbek",
-  age: 17,
-  job: "Developer",
-};
-person.name = "Vali";
-console.log(person);
+// const person = {
+//   name: "Asadbek",
+//   age: 17,
+//   job: "Developer",
+// };
+// person.name = "Vali";
+// console.log(person);
+
+//3-masala
+// const person = {
+//   name: "Asadbek",
+//   age: 17,
+//   job: "Developer",
+// };
+// person.place = "Farg`ona";
+// console.log(person);
+
+//4-masala
+// const person = {
+//   name: "Asadbek",
+//   age: 17,
+//   job: "Developer",
+// };
+// delete person.job;
+// person.place = "Farg`ona";
+// console.log(person);
+
+//5-masala
+// const person = {
+//   name: "Asadbek",
+//   age: 17,
+//   job: "Developer",
+// };
+// person.phone = ["+998991546441", 24];
+// console.log(person);
+
+// //6-masala
+// const person = {
+//   name: "Asadbek",
+//   age: 17,
+//   job: "Developer",
+// };
+// for (let name in person) console.log(name + ":" + person[name]);
+
+//7-masala
+// const person = {
+//   name: "Asadbek",
+//   age: 17,
+//   job: "Developer",
+// };
+// console.log(person.age);
+
+//8-masala
+// function makeObyekt(name, age, job) {
+//   return {
+//     name: name,
+//     age: age,
+//     job: job,
+//   };
+// }
+// const person = makeObyekt("Asadbek", 17, "Developer");
+// console.log(person);
+
+//Umumlashtiruvchi masalalar.
+//1-masala
+// const arr = "52433143";
+// function findElement(arr) {
+//   let sum = 0;
+//   for (let iterator of arr) {
+//     if (iterator === "3") {
+//       sum++;
+//     }
+//   }
+//   return sum;
+// }
+
+// console.log(findElement(arr));
+
+//2-masala
+// let arr = 1;
+// function createElement(arr, n) {
+//   let a = [];
+//   for (let i = arr; i < n; i++) {
+//     if (i % 2 == 1) {
+//       a.push(i);
+//     }
+//   }
+//   return a;
+// }
+// console.log(createElement(arr, 10));
+
+//3-masala
+
+//4-masala
