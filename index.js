e6fbf431dea0d4a780baaa360b801d18a8af0f04
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-06-26 17:50:49 +0500

    .

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Obyektlar aloqador masalala.
-
 //1-masala
 // const person = {
 //   name: "Asadbek",
@@ -7,7 +6,6 @@
 //   job: "Developer",
 // };
 // console.log(person);
-
 //2-masala
 // const person = {
 //   name: "Asadbek",
@@ -16,7 +14,6 @@
 // };
 // person.name = "Vali";
 // console.log(person);
-
 //3-masala
 // const person = {
 //   name: "Asadbek",
@@ -25,7 +22,6 @@
 // };
 // person.place = "Farg`ona";
 // console.log(person);
-
 //4-masala
 // const person = {
 //   name: "Asadbek",
@@ -35,7 +31,6 @@
 // delete person.job;
 // person.place = "Farg`ona";
 // console.log(person);
-
 //5-masala
 // const person = {
 //   name: "Asadbek",
@@ -44,7 +39,6 @@
 // };
 // person.phone = ["+998991546441", 24];
 // console.log(person);
-
 // //6-masala
 // const person = {
 //   name: "Asadbek",
@@ -52,7 +46,6 @@
 //   job: "Developer",
 // };
 // for (let name in person) console.log(name + ":" + person[name]);
-
 //7-masala
 // const person = {
 //   name: "Asadbek",
@@ -60,7 +53,6 @@
 //   job: "Developer",
 // };
 // console.log(person.age);
-
 //8-masala
 // function makeObyekt(name, age, job) {
 //   return {
@@ -71,7 +63,6 @@
 // }
 // const person = makeObyekt("Asadbek", 17, "Developer");
 // console.log(person);
-
 //Umumlashtiruvchi masalalar.
 //1-masala
 // const arr = "52433143";
@@ -84,9 +75,7 @@
 //   }
 //   return sum;
 // }
-
 // console.log(findElement(arr));
-
 //2-masala
 // let arr = 1;
 // function createElement(arr, n) {
@@ -99,7 +88,90 @@
 //   return a;
 // }
 // console.log(createElement(arr, 10));
-
 //3-masala
-
+// const str = "hel*l*o";
+// function removeItem(str) {
+//   let res = str.replace(/\*/g, "");
+//   return res;
+// }
+// console.log(removeItem(str));
 //4-masala
+// const str = "salom dunyo";
+// function removeSecondElement(str) {
+//   if (str.length < 2) {
+//     return str;
+//   }
+//   return str;
+// }
+// console.log(removeSecondElement(str));
+//5-masala
+// const arr = [1, 5, 7, 9, a, B];
+// function largeElement(arr) {
+//   arr.forEach(function () {
+//     arr.sort();
+//   });
+//   return arr;
+// }
+// console.log(largeElement(arr));
+
+//6-masala
+// const str = "hello";
+// function massivElement(str) {
+//   let res = str.split("");
+//   return res;
+// }
+// console.log(massivElement(str));
+
+//7-masala
+// const arr = [1, 2, 5, 6, 8];
+// function stringElement(arr) {
+//   let res = arr.join("");
+//   return res;
+// }
+// console.log(stringElement(arr));
+
+//8-masala
+
+//12-masala
+// const arr = ["h", "e", "l", "l", "o"];
+// function stringElement(arr) {
+//   let res = arr.reverse().join("");
+//   return res;
+// }
+// console.log(stringElement(arr));
+
+//13-masala
+// const str = "salom dunyo";
+// function massivElement(str) {
+//   let res = str.split(" ");
+//   return res;
+// }
+// console.log(massivElement(str));
+
+//14-masala
+// const str = "salom dunyo";
+// function newString(str) {
+//   let res = str.map(function (value) {
+//     return value.reverse();
+//   });
+//   return res;
+// }
+// console.log(newString(str));
+
+//15-masala
+// const str = ["salom", "dunyo"];
+// function newMassiv(str) {
+//   let res = str.join(" ").length;
+//   return res;
+// }
+// console.log(newMassiv(str));
+
+//18-masala
+// function toCapitalize(str) {
+//   let res = str.split(" ");
+//   res = res.map(function (value) {
+//     return value[0].toUpperCase() + value.slice(1);
+//   });
+//   return res.join(" ");
+// }
+// console.log(toCapitalize("hello world"));
